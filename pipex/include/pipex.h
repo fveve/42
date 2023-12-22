8cd521343a15fae301810aa8800ab99e24cd7132
fveve <adam.rafa@hotmail.com>
2023-12-22 14:54:47 +0100

    pipex

---
+++ pipex/include/pipex.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arafa <arafa@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/20 12:10:20 by arafa             #+#    #+#             */
+/*   Updated: 2023/12/22 11:44:15 by arafa            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <sys/wait.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <errno.h>
+# include <stdio.h>
+
+//Functions------------------------------------------------
+int ft_strlen(const char *s);
+int ft_strlen_tab(char **s);
+int	is_file(char *s);
+char	*ft_strdup(const char *s);
+#endif
