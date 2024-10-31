831f13b1ad59ea84edd758828fbd4cfd2ba2b940
fveve <adam.rafa@hotmail.com>
2024-10-31 15:57:26 +0100

    dsds

--- inception/srcs/requirements/mariadb/conf/init-db.sh
+++ inception/srcs/requirements/mariadb/conf/init-db.sh
@@ -1,40 +1,7 @@
-#!/bin/bash
-set -e
-
-# Start MariaDB in the background
-echo "Starting MariaDB..."
 service mysql start
 
-# Allow some time for the server to start
-sleep 10
-
-# Check if MariaDB started successfully
-if ! mysqladmin ping; then
-    echo "MariaDB failed to start!"
-    exit 1
-fi
-
-# Initialize the database
-echo "Initializing database..."
-
-# Create the database
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" || { echo "Failed to create database"; exit 1; }
-
-# Create the user and assign a password
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" || { echo "Failed to create user"; exit 1; }
-
-# Grant privileges to the user
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" || { echo "Failed to grant privileges"; exit 1; }
-
-# Modify the root user password
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';" || { echo "Failed to modify root user"; exit 1; }
-
-# Refresh privileges
-mysql -e "FLUSH PRIVILEGES;" || { echo "Failed to flush privileges"; exit 1; }
-
-# Indicate the script has completed
-echo "Database initialization completed."
-
-# Keep the container running by running the MariaDB server in the foreground
-echo "Starting MariaDB in foreground..."
-exec mysqld
+mysql -u root -e "CREATE DATABASE ${DB_NAME};"
+mysql -u root -e "CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';"
--- inception/srcs/requirements/wordpress/conf/auto-config.sh
+++ inception/srcs/requirements/wordpress/conf/auto-config.sh
@@ -1,7 +1,18 @@
-sleep 10
-wp config create	--allow-root \
-					--dbname=$SQL_DATABASE \
-					--dbuser=$SQL_USER \
-					--dbpass=$SQL_PASSWORD \
-					--dbhost=mariadb:3306 --path='/var/www/wordpress'
-/usr/sbin/php-fpm7.3 -F
+cd /var/www/wordpress
+wp core config	--dbhost=$DB_HOST \
+				--dbname=$DB_NAME \
+				--dbuser=$DB_USER \
+				--dbpass=$DB_PASSWORD \
+				--allow-root
+
+wp core install --title=$WP_TITLE \
+				--admin_user=$WP_ADMIN_USER \
+				--admin_password=$WP_ADMIN_PASSWORD \
+				--admin_email=$WP_ADMIN_MAIL \
+				--url=$WP_URL \
+				--allow-root
+
+wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
+cd -
+
+php-fpm7.3 -F
