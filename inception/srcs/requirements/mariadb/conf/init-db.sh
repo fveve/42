bfe2ff89257f9ede7b75831f8b332036e1e9a48a
fveve <adam.rafa@hotmail.com>
2024-11-04 12:56:11 +0100

    dsds

--- inception/srcs/requirements/mariadb/conf/init-db.sh
+++ inception/srcs/requirements/mariadb/conf/init-db.sh
@@ -8,10 +8,10 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;" 
 
 # Create the user if it does not exist
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';" 
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';" 
 
 # Grant all privileges to the user for the database
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'localhost';" 
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%';" 
 
 # Change the root user's password
 mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';" 
